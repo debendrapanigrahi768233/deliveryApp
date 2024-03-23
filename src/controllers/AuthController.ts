3c5f33e62d001b034c306dabc19f522f25247b64
DEBENDRA PANIGRAHI <debbhubaneswar123@gmail.com>
2024-03-23 18:52:19 +0530

    Add logger and error handling at auth contoller and services

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -3,17 +3,38 @@ import { Response } from "express";
 // import { User } from "../entity/User";
 import { RegisterUserRequest } from "../types";
 import { UserService } from "../services/UserService";
+import { NextFunction } from "express-serve-static-core";
+import { Logger } from "winston";
 
 export class AuthController {
   userService: UserService;
-  constructor(userService: UserService) {
+  logger: Logger;
+  constructor(userService: UserService, logger: Logger) {
     this.userService = userService;
+    this.logger = logger;
   }
 
-  async register(req: RegisterUserRequest, res: Response) {
+  async register(req: RegisterUserRequest, res: Response, next: NextFunction) {
     const { firstName, lastName, email, password } = req.body;
     // const userService = new UserService();
-    await this.userService.create({ firstName, lastName, email, password });
-    res.status(201).json();
+    this.logger.debug("Received details of the user to register", {
+      firstName,
+      lastName,
+      email,
+      password: "*******",
+    });
+    try {
+      const user = await this.userService.create({
+        firstName,
+        lastName,
+        email,
+        password,
+      });
+      this.logger.info("The user has been created", { id: user.id });
+      res.status(201).json({ id: user.id });
+    } catch (e) {
+      next(e);
+      return;
+    }
   }
 }
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -3,14 +3,17 @@ import { AuthController } from "../controllers/AuthController";
 import { UserService } from "../services/UserService";
 import { AppDataSource } from "../config/data-source";
 import { User } from "../entity/User";
+import logger from "../config/logger";
 
 const router = express.Router();
 
 //This three lines is called dependency injection where we are passing the dependency to the constructor from outside
 const userRepository = AppDataSource.getRepository(User);
 const userService = new UserService(userRepository);
-const authController = new AuthController(userService);
+const authController = new AuthController(userService, logger);
 
-router.post("/register", (req, res) => authController.register(req, res));
+router.post("/register", (req, res, next) =>
+  authController.register(req, res, next),
+);
 
 export default router;
